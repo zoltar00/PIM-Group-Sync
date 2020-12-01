05b1f5ab366cab35fed5ae82a198e876cbd4db82
zoltar00 <32526436+zoltar00@users.noreply.github.com>
2020-12-01 17:46:12 +0100

    Initial Commit

---
+++ Sync.ps1
@@ -0,0 +1,17 @@
+# ----------------------------------
+#
+# Desc: Synchronize an Active Directory Group to Azure AD PIM group
+#
+# V1.0
+#
+# -----------------------------------
+
+# Init param
+param(
+
+    $ADGroupName,
+    $AzureADGroupName
+
+)
+
+#Checking if exist
